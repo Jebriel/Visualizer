cff0f0fb9f04e896f59b821a8a0a3e39417887ea
Jebriel Abdul <jebrielabdul@Jebriels-MacBook-Pro.local>
2022-05-27 01:05:23 -0400

    added get_song_features; make get requests to get audio features of songs in playlists. Modified find_songs; added track_dict to init to easily store name of songs with id, function now creats the dict

--- Viz.py
+++ Viz.py
@@ -8,7 +8,10 @@ class SaveSongs:
         self.user_id = spotify_user_id
         self.user_token = Refresh().newToken
         self.playlist_id = playlist_id
-        self.tracks =''
+        self.tracks_uri =''
+        self.tracks_name = ''
+        self.num_tracks = 0
+        self.track_dict = {}
 
     def find_songs(self):
         #get songs in playlits
@@ -21,12 +24,33 @@ class SaveSongs:
                                 )
         response_json = response.json()
         print(response_json.keys())
+        self.num_tracks = len(response_json['items'])
         for i in response_json['items']:
-            self.tracks+=(i['track']['name']+',')
-     
-
+            self.tracks_uri+=(i['track']['uri']+',').split(':')[-1]
+            self.tracks_name+=(i['track']['name']+',').split(':')[-1]
+        self.tracks_uri = self.tracks_uri.split(',')
+        self.tracks_name = self.tracks_name.split(',')
+        self.track_dict = {self.tracks_name[i]: [self.tracks_uri[i]] for i in range(self.num_tracks)}
+        return
         #self.tracks=self.tracks[:-1]
 
+    def get_song_features(self):
+        self.find_songs()
+        print('Taking audio features')
+        for track in self.track_dict.keys():
+            song = self.track_dict[track][0]
+            query = 'https://api.spotify.com/v1/audio-features/{}'.format(song)
+            response = requests.get(query, 
+                                headers = {'content-Type':'application/json',
+                                            'Authorization':'Bearer {}'.format(self.user_token)
+                                            }
+                               )
+            self.track_dict[track].append(response.json())
+            #print(response.json(),'-'*30)
+        return
+
 a = SaveSongs()
-a.find_songs()
-print(a.tracks)
+#a.find_songs()
+a.get_song_features()
+info = a.track_dict
+print(info['Succession (Main Title Theme) - Extended Intro Version'])
